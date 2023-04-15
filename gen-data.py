0c4c165bcf646c931200ab5a297565c7daa271d2
Alex Maksimov <maleksware@gmail.com>
2023-04-15 10:10:09 +1000

    Add data generation script

---
+++ gen-data.py
@@ -0,0 +1,37 @@
+import random
+
+n = 1000
+max_line_length = 1000
+
+min_y = 0
+max_y = 10000
+
+lines = []
+
+for _ in range(n):
+    line = []
+    min_x = 0
+    max_x = 10000
+
+    line.append((0, random.randint(min_y, max_y)))
+
+    for i in range(random.randint(2, max_line_length)):
+        line.append((random.randint(min_x + 1, max_x - 1), random.randint(min_y, max_y)))
+        min_x = line[-1][0]
+        if min_x == max_x - 1: break
+
+    line.append((max_x, random.randint(min_y, max_y)))
+
+    lines.append(line)
+
+
+with open("data/lines.txt", "w") as fout:
+    print(n, file=fout)
+    print(file=fout)
+
+    for line in lines:
+        for p in line:
+            print(str(p[0]) + ',' + str(p[1]), file=fout)
+        print(file=fout)
+
+    
